646ac8d548860fcb80abb258efc515abc47dc33c
Daniel <guardians01@outlook.com>
2024-03-13 19:24:23 -0600

    useNavigate

--- src/ui/components/NavBar.jsx
+++ src/ui/components/NavBar.jsx
@@ -1,7 +1,15 @@
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 
 
 export const NavBar = () => {
+    const navigate = useNavigate();
+    
+    const onLogout = () =>{
+        navigate('/login', {
+            replace:true
+        })
+    }
+
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-3">
             
@@ -35,7 +43,12 @@ export const NavBar = () => {
                 <ul className="navbar-nav ml-auto">
                     <span className="nav-item nav-link text-info"> Daniel </span>
 
-                    <button className="nav-item nav-link btn btn-dark"> Cerrar sesión </button>
+                    <button 
+                        className="nav-item nav-link btn btn-dark"
+                        onClick={ onLogout }
+                    > Cerrar sesión 
+                    
+                    </button>
                     
                 </ul>
             </div>
